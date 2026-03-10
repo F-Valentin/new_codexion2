0f1b5f89c4c7ab8a0a904949e843b511267d1a4c
Valentin Fechter <vafechte@k1r1p10.42mulhouse.fr>
2026-03-10 16:24:17 +0100

    free data cond and mutex

--- coders/src/init.c
+++ coders/src/init.c
@@ -6,7 +6,7 @@
 /*   By: vafechte <vafechte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 16:49:46 by vafechte          #+#    #+#             */
-/*   Updated: 2026/03/10 16:23:05 by vafechte         ###   ########.fr       */
+/*   Updated: 2026/03/10 16:24:14 by vafechte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ static bool	data_init(t_data *data)
 	return (true);
 }
 
-// pas oublier de free les mutex de data init
 bool	init(t_data *data)
 {
 	if (!data)
