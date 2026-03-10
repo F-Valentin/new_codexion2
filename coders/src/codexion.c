5acc134e07f1b0f9540a99bac283158a9dfd5dc5
Valentin Fechter <vafechte@k1r1p10.42mulhouse.fr>
2026-03-10 15:42:44 +0100

    print data

--- coders/src/codexion.c
+++ coders/src/codexion.c
@@ -6,7 +6,7 @@
 /*   By: vafechte <vafechte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 11:11:28 by vafechte          #+#    #+#             */
-/*   Updated: 2026/03/10 14:39:13 by vafechte         ###   ########.fr       */
+/*   Updated: 2026/03/10 15:38:10 by vafechte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,14 @@ int	main(int ac, char **av)
 		fprintf(stderr, "Parsing input failed.\n");
 		return (-1);
 	}
+	printf("nb of coders: %d\n", data.number_of_coders);
+	printf("time_to_burnout: %ld\n", data.time_to_burnout);
+	printf("time_to_compile: %ld\n", data.time_to_compile);
+	printf("time_to_debug: %ld\n", data.time_to_debug);
+	printf("time_to_refactor: %ld\n", data.time_to_refactor);
+	printf("nb of compile required: %d\n", data.number_of_coders);
+	printf("dongle cooldown: %ld\n", data.dongle_cooldown);
+	printf("scheduler: %s\n", data.scheduler == 0 ? "fifo" : "edf");
 	if (!init(&data))
 	{
 		fprintf(stderr, "Init data failed.\n");
