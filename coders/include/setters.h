3889fca6d7e46979dbf2a3bd45a402217d1a210e
Valentin Fechter <vafechte@k1r1p11.42mulhouse.fr>
2026-03-12 09:43:12 +0100

    impair re

--- coders/include/setters.h
+++ coders/include/setters.h
@@ -1,28 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   setters.c                                          :+:      :+:    :+:   */
+/*   setters.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vafechte <vafechte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/12 09:28:11 by vafechte          #+#    #+#             */
-/*   Updated: 2026/03/12 09:28:21 by vafechte         ###   ########.fr       */
+/*   Created: 2026/03/12 09:29:08 by vafechte          #+#    #+#             */
+/*   Updated: 2026/03/12 09:29:09 by vafechte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "codexion.h"
+#ifndef SETTERS_H
+# define SETTERS_H
 
-void	set_long(long *v, long s, pthread_mutex_t *mutex)
-{
-	pthread_mutex_lock(mutex);
-	*v = s;
-	pthread_mutex_unlock(mutex);
-}
+# include <pthread.h>
 
-
-void	set_int(int *v, int s, pthread_mutex_t *mutex)
-{
-	pthread_mutex_lock(mutex);
-	*v = s;
-	pthread_mutex_unlock(mutex);
-}
+void	set_int(int *v, int s, pthread_mutex_t *mutex);
+void	set_long(long *v, long s, pthread_mutex_t *mutex);
+#endif
--- coders/src/utils.c
+++ coders/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: vafechte <vafechte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 14:38:57 by vafechte          #+#    #+#             */
-/*   Updated: 2026/03/11 14:59:11 by vafechte         ###   ########.fr       */
+/*   Updated: 2026/03/12 09:42:41 by vafechte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ void	log_status(t_coder *coder, const char *status)
 {
 	long	timestamp;
 
-	// peut etre race condition avec le monitor pour afficher le burnout
+	pthread_mutex_lock(&coder->data->data_mutex);
 	timestamp = get_time_in_ms() - coder->data->start_time;
+	pthread_mutex_unlock(&coder->data->data_mutex);
 	if (!is_simulation_finished(coder->data))
 	{
 		pthread_mutex_lock(&coder->data->log_mutex);
